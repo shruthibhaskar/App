fa5b429ba52dc7c278693c27049189c79b0d7347
shruthibhaskar <34641935+shruthibhaskar@users.noreply.github.com>
2018-03-11 20:34:57 +0530

    Update server3.js

--- server3.js
+++ server3.js
@@ -9,7 +9,7 @@ http.createServer(function (request, response) {
    
    // Send the response body as "Hello World"
    response.end('<h2>My Third Node App<h2><br />');
-}).listen(3003);
+}).listen(3004);
 
 // Console will print the message
-console.log('Server running 3003');
+console.log('Server running 3004');
