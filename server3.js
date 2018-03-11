ca4b0ff23614e18d7e2a8152c30c30f00eaf1108
shruthibhaskar <34641935+shruthibhaskar@users.noreply.github.com>
2018-03-11 20:29:06 +0530

    Add files via upload

---
+++ server3.js
@@ -0,0 +1,15 @@
+var http = require("http");
+
+http.createServer(function (request, response) {
+
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/html
+   response.writeHead(200, {'Content-Type': 'text/html'});
+   
+   // Send the response body as "Hello World"
+   response.end('<h2>My Third Node App<h2><br />');
+}).listen(3003);
+
+// Console will print the message
+console.log('Server running 3003');
