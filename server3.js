34ae93db001af28541c7b4b7158033e0c7bda45b
shruthibhaskar <34641935+shruthibhaskar@users.noreply.github.com>
2018-03-14 17:26:16 +0530

    Update server3.js

--- server3.js
+++ server3.js
@@ -9,7 +9,7 @@ http.createServer(function (request, response) {
    
    // Send the response body as "Hello World"
    response.end('<h2>My Third Node App<h2><br />');
-}).listen(3004);
+}).listen(3005);
 
 // Console will print the message
-console.log('Server running on port3004');
+console.log('Server running on port 3005');
