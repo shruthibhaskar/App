e43c8915d5012e1704dfece3cc38e4a0a3a3a802
shruthibhaskar <34641935+shruthibhaskar@users.noreply.github.com>
2018-03-15 22:40:44 +0530

    Update server3.js

--- server3.js
+++ server3.js
@@ -12,4 +12,4 @@ http.createServer(function (request, response) {
 }).listen(3003);
 
 // Console will print the message
-console.log('Server running on port 3003 from Jenkins : test1');
+console.log('Server running on port 3003 from Jenkins : test2');
