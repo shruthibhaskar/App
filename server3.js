3cd65208a3762791c74967b509b5ee373b6a467b
shruthibhaskar <34641935+shruthibhaskar@users.noreply.github.com>
2018-04-05 11:52:41 +0530

    Update server3.js

--- server3.js
+++ server3.js
@@ -8,8 +8,8 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/html'});
    
    // Send the response body as "Hello World"
-   response.end('<h2>My Third Node App -Jenkins - test - demo Pipeline Jenkins test 1 <h2><br />');
+   response.end('<h2>My Third Node App -Jenkins - test - demo Pipeline Jenkins test 2 <h2><br />');
 }).listen(3003);
 
 // Console will print the message
-console.log('Server running on port 3003 from Jenkins :demo Pipeline test 1');
+console.log('Server running on port 3003 from Jenkins :demo Pipeline test 2');
